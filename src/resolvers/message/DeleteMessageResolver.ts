102451b1470ccddf3f929b0b84402920aa13446b
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-09-13 17:41:48 +0200

    added authorization to delete message mutation

--- src/resolvers/message/DeleteMessageResolver.ts
+++ src/resolvers/message/DeleteMessageResolver.ts
@@ -1,18 +1,24 @@
 import { ApolloError } from 'apollo-server-express';
-import { Arg, Args, Mutation, Resolver } from 'type-graphql';
+import { Arg, Args, Ctx, Mutation, Resolver } from 'type-graphql';
+import { Context } from '../../interfaces';
 import { Message, MessageModel } from '../../models';
+import { getUserId } from '../../utils';
 import { DeleteMessageArgs } from './DeleteMessageArgs';
 
 @Resolver()
 export class DeleteMessageResolver {
   @Mutation(() => Message)
-  async deleteMessage(@Args() { id }: DeleteMessageArgs): Promise<Message> {
+  async deleteMessage(
+    @Args() { id }: DeleteMessageArgs,
+    @Ctx() { req }: Context
+  ): Promise<Message> {
     // TODO => Authorization here
+    const userId = getUserId(req);
+    const message = await MessageModel.findById(id);
+    if (!message || message.author !== userId)
+      throw new ApolloError('Authorization required');
     // TODO => Cascade delete
-
-    const message = await MessageModel.findByIdAndDelete(id);
-
-    if (!message) throw new ApolloError("Couldn't find message");
+    await MessageModel.remove(message._id);
 
     return message;
   }
