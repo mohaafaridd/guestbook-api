42a4e7cce0216223fa8e4c46d46a5b6fd5a59cd6
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-09-13 17:18:25 +0200

    created a utility to extract user id

---
+++ src/utils/getUserId.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import { Request } from 'express';
+import { ApolloError } from 'apollo-server-express';
+
+export const getUserId = (request: Request, requireAuth = true) => {
+  const header = request.headers.authorization;
+
+  if (header) {
+    const token = header.replace('Bearer ', '');
+    const decoded: any = jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'hard jwt secret'
+    );
+    return decoded.userId;
+  }
+
+  if (requireAuth) {
+    throw new ApolloError('Authorization required');
+  }
+
+  return undefined;
+};
